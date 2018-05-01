24339b8248686130a7a32c92f2adab327f957228
GatoY <derakhy@gmail.com>
2018-05-02 00:10:29 +1000

    merge

--- mediaMap/map/views.py
+++ mediaMap/map/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from couchdb import Server
+from couchdb.client import ResourceNotFound
+
+SERVER = Server('http://127.0.0.1:5984')
+if(len(SERVER)==0):
+    SERVER.create()
 
 def index(request):
     return render(request,'map/index.html')
                   
 def map(request):
-    return render(request,'map/map.html')
+    return render(request,'map/chart.html')
 # Create your views here.
