9ac49f4d0fe13664bc20de3e426b590800baa250
GatoY <derakhy@gmail.com>
2018-05-02 15:35:43 +1000

    boto key.py

---
+++ scripts/boto/key.py
@@ -0,0 +1,9 @@
+class key():
+    my_access_key_id = '79085a938ed84af3b2ec16fec9fb856f'
+    my_secret_access_key = 'fca75136622f4fa0a33707620ca3b74f'
+    def __init__(self):
+    	pass
+    def get_id(self):
+        return self.my_access_key_id
+    def get_key(self):
+        return self.my_secret_access_key 
