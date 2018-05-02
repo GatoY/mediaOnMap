859c2f75db529734bc38ff540a1272942dad6e6c
GatoY <derakhy@gmail.com>
2018-05-02 15:04:51 +1000

    staticfiles set and mkdir for other parts of code

--- mediaMap/map/urls.py
+++ mediaMap/map/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('map', views.map, name='map'),
+    path('/index', views.index, name='index'),
+    path('/map', views.map, name='map'),
 ]
