94be8bbc80cb8a88a47f48be99ba3267d9480aed
dongjize <justecommeca@163.com>
2018-05-05 15:52:21 +1000

    remove async defer

--- web/mediaMap/map/urls.py
+++ web/mediaMap/map/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
+    path('sentiment', views.sentiment, name='sentiment'),
     path('scenario1', views.scenario1, name='scenario1'),
     path('scenario2', views.scenario2, name='scenario2'),
     path('scenario3', views.scenario3, name='scenario3'),
