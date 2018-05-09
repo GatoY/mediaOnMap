f8137677df9f280293dd232bcdce776d3d2639c6
dongjize <justecommeca@163.com>
2018-05-09 21:10:00 +1000

    fix

--- web/mediaMap/map/dataMappers/sentiment_by_suburb.py
+++ web/mediaMap/map/dataMappers/sentiment_by_suburb.py
@@ -1,23 +1,25 @@
 import json
 
-with open('../static/map/img/melbourne_suburb.geojson') as f:
+with open('../static/map/img/sentiment_by_suburbs.geojson') as f:
     geojson = json.load(f)
 
     for feature in geojson["features"]:
-        sentiment = feature["properties"]["pos_rate"]
+        properties = feature["properties"]
+
+        sentiment = properties["pos_rate"]
 
         if sentiment is None:
-            feature["properties"]["color"] = "#666666"
+            properties["color"] = "#666666"
         elif float(sentiment) < 0.2:
-            feature["properties"]["color"] = "#FF3333"
+            properties["color"] = "#FF3333"
         elif float(sentiment) < 0.4:
-            feature["properties"]["color"] = "#FF9933"
+            properties["color"] = "#FF9933"
         elif float(sentiment) < 0.6:
-            feature["properties"]["color"] = "#FFFF33"
+            properties["color"] = "#FFFF33"
         elif float(sentiment) < 0.8:
-            feature["properties"]["color"] = "#99FF33"
+            properties["color"] = "#99FF33"
         else:
-            feature["properties"]["color"] = "#00BB00"
+            properties["color"] = "#00BB00"
 
     with open('../static/map/res/melbourne_suburbs.geojson', 'w') as outFile:
         json.dump(geojson, outFile)
